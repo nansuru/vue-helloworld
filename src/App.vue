2af5c3990f768f81261398ec7f61773da10cb315
DESKTOP-H3L3I4U\ficti <madebyrh@gmail.com>
2021-01-10 20:10:54 +0900

    list3-21-22

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <HelloWorld v-bind:title="message" />
+    <HelloWorld v-bind:title="message" v-on:result-event="appAction" />
     <hr />
-    <button class="btn btn-primary" v-on:click="doAction">change title</button>
+    <p>{{ result }}</p>
   </div>
 </template>
 
@@ -17,12 +17,12 @@ export default {
   data() {
     return {
       message: "HELLO",
+      result: "no event.",
     };
   },
   methods: {
-    doAction() {
-      var input = prompt("new title:");
-      this.message = input;
+    appAction(message) {
+      this.result = '(*** you send:"' + message + '".***)';
     },
   },
 };
